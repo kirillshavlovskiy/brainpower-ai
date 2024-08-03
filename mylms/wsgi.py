a581c58e9a1865fb268a19007a696f679003e2c4
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-08-03 16:20:12 +0300

    added wsgi.py corrections

--- mylms/wsgi.py
+++ mylms/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LMS_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylms.settings')
 
 application = get_wsgi_application()

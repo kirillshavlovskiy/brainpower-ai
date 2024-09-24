55b9eb7f116621a8ac2d8c7f32ffe4af71036622
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-09-24 23:49:29 +0300

    new deployment logic

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import DeployToProductionView_dev, DeployToProductionView_prod
+from .views import DeployToProductionView_dev
 import os
 
 urlpatterns = [
@@ -12,6 +12,5 @@ urlpatterns = [
     path('stop_container/', views.stop_container, name='stop_container'),
     path('update_code/', views.update_code, name='update_code'),
     path('deploy_to_production/', DeployToProductionView_dev.as_view(), name='deploy_to_production'),
-    path('deploy_to_server/', DeployToProductionView_prod.as_view(), name='deploy_to_server'),
     path('check_container_ready/', views.check_container_ready, name='check_container_ready'),
 ] + static(settings.DEPLOYED_COMPONENTS_URL, document_root=settings.DEPLOYED_COMPONENTS_ROOT)

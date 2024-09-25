af6c061b4f7b5f1c37b61a3571b308dea85ae545
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-09-25 23:41:40 +0300

    new deployment logic

--- courses/consumers.py
+++ courses/consumers.py
@@ -23,27 +23,6 @@ import shutil
 User = get_user_model()
 logger = logging.getLogger(__name__)
 
-class DeploymentConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.task_id = self.scope['url_route']['kwargs']['task_id']
-        self.group_name = f"deployment_{self.task_id}"
-
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
-
-    async def deployment_update(self, event):
-        await self.send(text_data=json.dumps(event))
-
 class AsyncChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = 'chat'
--- courses/routing.py
+++ courses/routing.py
@@ -4,5 +4,4 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'^ws/livechat_autogen/$', consumers.AsyncChatConsumer.as_asgi()),
     re_path(r'ws/file_structure/$', consumers.FileStructureConsumer.as_asgi()),
-    re_path(r'ws/deployment/(?P<task_id>\w+)/$', consumers.DeploymentConsumer.as_asgi()),
 ]

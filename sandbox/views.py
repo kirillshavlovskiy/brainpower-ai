8e5d1f0cc27af475a52bbc76f564812a24670094
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-09-26 01:09:47 +0300

    new deployment logic

--- sandbox/views.py
+++ sandbox/views.py
@@ -530,7 +530,8 @@ class ServeReactApp(TemplateView):
         template_path = f'{settings.DEPLOYED_COMPONENTS_ROOT}/{app_name}/index.html'
         return [template_path]
 
-
+import pwd
+import grp
 import os
 import json
 import asyncio
@@ -585,19 +586,21 @@ class DeployToProductionView_prod(View):
             app_name = f"{user_id}_{file_name.replace('.', '-')}"
             production_dir = os.path.join(settings.DEPLOYED_COMPONENTS_ROOT, app_name)
 
-            # # Stop the yarn start process
-            # container.exec_run(["sh", "-c", "RUN apt-get update && apt-get install -y procps"])
-            #
-            # self.send_update(channel_layer, task_id, "Stopping yarn start process...")
-            # stop_command = "pkill -f 'react-scripts start'"
-            # exec_result = container.exec_run(["sh", "-c", stop_command])
-            # if exec_result.exit_code != 0:
-            #     logger.warning(f"Failed to stop yarn start: {exec_result.output.decode()}")
+            # Stop the yarn start process
+            container.exec_run(["sh", "-c", "RUN apt-get update && apt-get install -y procps"])
+
+            self.send_update(channel_layer, task_id, "Stopping yarn start process...")
+            stop_command = "pkill -f 'react-scripts start'"
+            exec_result = container.exec_run(["sh", "-c", stop_command])
+            if exec_result.exit_code != 0:
+                logger.warning(f"Failed to stop yarn start: {exec_result.output.decode()}")
 
             # Start production build
             self.send_update(channel_layer, task_id, "Starting production build...")
             build_command = f"""
-            
+            RUN apt-get update && apt-get install -y procps
+            export NODE_OPTIONS="--max-old-space-size=8192" && \
+            export GENERATE_SOURCEMAP=false && \
             export PUBLIC_URL="/deployed_apps/{app_name}" && \
             yarn build
             """
@@ -616,8 +619,11 @@ class DeployToProductionView_prod(View):
             if copy_result.returncode != 0:
                 raise Exception(f"Failed to copy build files: {copy_result.stderr}")
 
-            # Set file permissions
+            # Set file ownership and permissions
             self.send_update(channel_layer, task_id, "Setting file permissions...")
+            uid = pwd.getpwnam('www-data').pw_uid
+            gid = grp.getgrnam('www-data').gr_gid
+
             for root, dirs, files in os.walk(production_dir):
                 for dir in dirs:
                     os.chmod(os.path.join(root, dir), 0o755)

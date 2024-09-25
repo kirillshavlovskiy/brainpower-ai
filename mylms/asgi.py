09dfa358254bf8c6f80ad29dcfdc9912a0811387
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-09-26 00:48:31 +0300

    new deployment logic

--- mylms/asgi.py
+++ mylms/asgi.py
@@ -1,19 +1,24 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from sandbox import routing as sandbox_routing
-from courses import routing as courses_routing
-from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylms.settings')
+
+# Initialize Django
+django.setup()
+
+# Now import your custom middleware and routing
+from courses.middleware import UserIDAuthMiddleware
+from sandbox import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            sandbox_routing.websocket_urlpatterns +
-            courses_routing.websocket_urlpatterns
-        )
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
     ),
 })

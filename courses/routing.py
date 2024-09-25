4724ce65ffd9bf9fff9d81413069a5e01c25fbeb
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-09-26 00:44:28 +0300

    new deployment logic

--- courses/routing.py
+++ courses/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/livechat_autogen/$', consumers.AsyncChatConsumer.as_asgi()),
-    re_path(r'ws/file_structure/$', consumers.FileStructureConsumer.as_asgi()),
+    re_path(r'^ws/courses/livechat_autogen/$', consumers.AsyncChatConsumer.as_asgi()),
+    re_path(r'ws/courses/file_structure/$', consumers.FileStructureConsumer.as_asgi()),
 ]
--- mylms/asgi.py
+++ mylms/asgi.py
@@ -1,24 +1,19 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from sandbox import routing as sandbox_routing
+from courses import routing as courses_routing
+from django.urls import path
 
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylms.settings')
-
-# Initialize Django
-django.setup()
-
-# Now import your custom middleware and routing
-from courses.middleware import UserIDAuthMiddleware
-from courses import routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
+        URLRouter(
+            sandbox_routing.websocket_urlpatterns +
+            courses_routing.websocket_urlpatterns
+        )
     ),
 })

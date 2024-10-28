0cf80b5ca1b4147a7f7c09f3e4a434c3045e3155
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-10-28 05:09:02 +0300

    new container update logic

--- sandbox/views.py
+++ sandbox/views.py
@@ -808,13 +808,13 @@ def check_or_create_container(request):
                     'NODE_OPTIONS': '--max-old-space-size=8192'
                 },
                 volumes={
-                    f"{react_renderer_path}/package.json": {'bind': '/app/package.json', 'mode': 'ro'},
-                    f"{react_renderer_path}/tsconfig.json": {'bind': '/app/tsconfig.json', 'mode': 'ro'},
-                    f"{react_renderer_path}/tailwind.config.json": {'bind': '/app/tsconfig.json', 'mode': 'ro'},
-                    f"{react_renderer_path}/public": {'bind': '/app/public', 'mode': 'ro'},
-                    f"{react_renderer_path}/src/index.js": {'bind': '/app/src/index.js', 'mode': 'ro'},
-                    f"{react_renderer_path}/src/index.css": {'bind': '/app/src/index.css', 'mode': 'ro'},
-                    f"{react_renderer_path}/postcss.config.js": {'bind': '/app/postcss.config.js', 'mode': 'ro'},
+                    f"{react_renderer_path}/package.json": {'bind': '/app/package.json', 'mode': 'rw'},
+                    f"{react_renderer_path}/tsconfig.json": {'bind': '/app/tsconfig.json', 'mode': 'rw'},
+                    f"{react_renderer_path}/tailwind.config.json": {'bind': '/app/tsconfig.json', 'mode': 'rw'},
+                    f"{react_renderer_path}/public": {'bind': '/app/public', 'mode': 'rw'},
+                    f"{react_renderer_path}/src/index.js": {'bind': '/app/src/index.js', 'mode': 'rw'},
+                    f"{react_renderer_path}/src/index.css": {'bind': '/app/src/index.css', 'mode': 'rw'},
+                    f"{react_renderer_path}/postcss.config.js": {'bind': '/app/postcss.config.js', 'mode': 'rw'},
                 #
                 },
                 ports={'3001/tcp': host_port},

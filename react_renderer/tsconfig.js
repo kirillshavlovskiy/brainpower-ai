277f0491228393274c132a035dd320dabec6fd92
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-10-29 15:53:01 +0300

    new container update logic

---
+++ react_renderer/tsconfig.js
@@ -0,0 +1,36 @@
+{
+  "compilerOptions": {
+      "baseUrl": ".",
+        "paths": {
+          "@/*": ["./*"]
+        },
+        "target": "es5",
+        "lib": ["dom", "dom.iterable", "esnext"],
+        "allowJs": true,
+        "skipLibCheck": true,
+        "esModuleInterop": true,
+        "allowSyntheticDefaultImports": true,
+        "strict": true,
+        "forceConsistentCasingInFileNames": true,
+        "noFallthroughCasesInSwitch": true,
+        "module": "esnext",
+        "moduleResolution": "node",
+        "resolveJsonModule": true,
+        "isolatedModules": true,
+        "noEmit": false,
+        "jsx": "react-jsx",
+
+        "outDir": "./dist",
+        "sourceMap": true,
+        "declaration": true,
+        "incremental": true
+  },
+  "include": ["src/**/*"],
+  "exclude": [
+    "node_modules",
+    "build",
+    "dist",
+    "scripts",
+    "webpack.config.js"
+  ]
+}
--- sandbox/views.py
+++ sandbox/views.py
@@ -847,7 +847,8 @@ def check_or_create_container(request):
                 f"{react_renderer_path}/build": {'bind': '/app/build', 'mode': 'rw'},
                 # Configuration files
                 f"{react_renderer_path}/package.json": {'bind': '/app/package.json', 'mode': 'rw'},
-                # Tailwind configuration
+                # Tailwind/TS configuration
+                f"{react_renderer_path}/tsconfig.json": {'bind': '/app/tsconfig.json', 'mode': 'rw'},
                 f"{react_renderer_path}/tailwind.config.js": {'bind': '/app/tailwind.config.js', 'mode': 'rw'},
                 f"{react_renderer_path}/postcss.config.js": {'bind': '/app/postcss.config.js', 'mode': 'rw'},
              }

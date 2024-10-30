f153a0a6210075c6b3f50a538fa9b44e517d4241
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-10-30 18:27:12 +0300

    new container update logic

--- sandbox/views.py
+++ sandbox/views.py
@@ -915,7 +915,7 @@ def check_or_create_container(request):
                 f"{react_renderer_path}/src/index.js": {'bind': '/app/src/index.js', 'mode': 'rw'},
                 f"{react_renderer_path}/src/index.css": {'bind': '/app/src/index.css', 'mode': 'rw'},
                 f"{react_renderer_path}/src/lib/utils.ts": {'bind': '/app/src/lib/utils.ts', 'mode': 'rw'},
-                f"{react_renderer_path}/components/ui/button": {'bind': '/app/src/components/button', 'mode': 'rw'},
+                f"{react_renderer_path}/components/ui/button/index.tsx": {'bind': '/app/src/components/button/index.tsx', 'mode': 'rw'},
 
                 f"{react_renderer_path}/public": {'bind': '/app/public', 'mode': 'rw'},
                 f"{react_renderer_path}/build": {'bind': '/app/build', 'mode': 'rw'},
